76e625fd8c4b6c5828c4a934df8535ed7a4fd59d
imminh20x <imminh20x@gmail.com>
2025-11-03 17:31:07 +0700

    add DELETE method

---
+++ tests/DELETE_METHOD.spec.js
@@ -0,0 +1,15 @@
+const { test, expect } = require("@playwright/test");
+
+test('should be able to delete', async ({request})=>{
+    // construct data
+    const url = 'https://jsonplaceholder.typicode.com/posts/1'
+
+    // send DELETE request
+    const response = await request.delete(url)
+    const status = response.status()
+    // stdout
+    console.log(status)
+
+    // verification
+    expect(status).toBe(200)
+})
